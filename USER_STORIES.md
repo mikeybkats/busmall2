d66ac8941fe594df2c365303b3ed80d3cbafbe3c
Michael Shipley Barakat <michaebkats@gmail.com>
2016-06-05 13:01:06 -0700

    first commit

---
+++ USER_STORIES.md
@@ -0,0 +1,10 @@
+# User Stories
+## Four to five user stories for each stakeholder:
+#### Stakeholder 1 - Market research team
+The market research team needs an application that creates data for them that is transparent and accurate because they will be using it to develop new action items for the product team.
+
+#### Stakeholder 2 - Development team
+The development team should make a product that is light and efficient and lends itself to a streamlined development process. The technology component of the project needs to have fewer complex parts and clean readable code that could be scaled for future growth.
+
+#### Stakeholder 3 - Focus group participant
+Focus group participants will require an application whose use is completely transparent. Their experience of using should include powerful descriptors and cognitive walkthroughs in addition to the photographs. This will ensure a more secure route to conversion at a later date. 
---
+++ app.js
@@ -0,0 +1,48 @@
+var imageLeft = document.getElementById('left');
+var imageCenter = document.getElementById('center');
+var imageRight = document.getElementById('right');
+var allProducts = [];
+var picNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+function Product(name){
+  this.name = name;
+  this.clicks = 0;
+  this.views = 0;
+  this.path = 'images/' + name + '.jpg';
+}
+
+function pushToProductArray (){
+  for (var i = 0; i < picNames.length; i++){
+    allProducts.push(new Product(picNames[i]));
+  }
+}
+
+function randNum (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function imageLoad(){
+  var indexLeft = randNum(0, allProducts.length);
+  imageLeft.src = allProducts[indexLeft].path;
+
+  var indexCenter = randNum(0, allProducts.length);
+  imageCenter.src = allProducts[indexCenter].path;
+  if (indexCenter.src === indexLeft.src){
+    var indexCenter = randNum(0, allProducts.length);
+    imageCenter.src = allProducts[indexCenter].path;
+  }
+
+  var indexRight = randNum(0, allProducts.length);
+  imageRight.src = allProducts[indexRight].path;
+}
+
+function imageClick(event){
+
+}
+
+pushToProductArray();
+pushToProductArray();
+imageLoad();
+
+// set up click event to reload images and log clicks
+// set up a views logger so that ever time an image is loaded we tally its views
